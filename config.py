460e364ce1a2b92a21511d285e5000be6c468af6
iksnizez <jrbrzezin@gmail.com>
2025-08-22 23:22:00 -0400

    vbd replacement player calcs ready

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 from pathlib import Path
 from dotenv import load_dotenv
-import os
+import os, sys
 
 def get_project_root():
     """
@@ -10,24 +10,31 @@ def get_project_root():
     """
     try:
         # Running from a file on disk
-        return Path(__file__).resolve().parent.parent   # the number of parents should be how many levels below the root the config is saved
+        return Path(__file__).resolve().parent   # the number of parents should be how many levels below the root the config is saved
     except NameError:
         # Interactive mode (IPython, Jupyter, or plain Python shell)
-        return Path.cwd().parent if Path.cwd().name == "scripts" else Path.cwd()
+        return Path.cwd().parent if Path.cwd().name == "notebooks" else Path.cwd()
 
 PROJECT_ROOT = get_project_root()
 
 # Load environment variables without overwriting existing ones
 ENV_PATH = PROJECT_ROOT / '.env'
 load_dotenv(dotenv_path=ENV_PATH, override=False)
-
-# config data 
-DATA_DIR = PROJECT_ROOT / 'data'
-BROWSER_DIR = PROJECT_ROOT / 'browsers'
 ESPN_COOKIES_SWID = os.environ.get('ESPN_COOKIES_SWID')
 ESPN_COOKIES_S2 = os.environ.get('ESPN_COOKIES_S2')
 ESPN_HEADERS_NAME=os.environ.get('ESPN_HEADERS_NAME')
 ESPN_HEADERS=os.environ.get('ESPN_HEADERS')
 ESPN_LEAGUE_ID = os.environ.get('ESPN_LEAGUE_ID')
 PYMYSQL_NFL = os.environ.get('PYMYSQL_NFL')
+
+# config data 
+DATA_DIR = PROJECT_ROOT / 'data'
+BROWSER_DIR = PROJECT_ROOT / 'browsers'
+MODULES_DIR = PROJECT_ROOT / 'modules'
+
+# add modules to the temp system path so notebooks and scripts can easily import
+if MODULES_DIR not in sys.path:
+    sys.path.append(MODULES_DIR)
+
+
 
